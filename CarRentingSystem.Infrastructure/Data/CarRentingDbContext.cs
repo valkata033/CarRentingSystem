84c197b5aaaf6b979f0d1b98240c40960729d286
Valentin <valkata033@abv.bg>
2022-11-16 14:27:44 +0200

    Added Initial migraion

--- CarRentingSystem.Infrastructure/Data/CarRentingDbContext.cs
+++ CarRentingSystem.Infrastructure/Data/CarRentingDbContext.cs
@@ -9,5 +9,10 @@ namespace CarRentingSystem.Infrastructure.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
