8fa080ec0babd3cc22d1b47c0bcd369902cf8f93
Valentin <valkata033@abv.bg>
2022-11-16 14:03:06 +0200

    Initial architecture setup

---
+++ CarRentingSystem.Infrastructure/Data/CarRentingDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentingSystem.Infrastructure.Data
+{
+    public class CarRentingDbContext : IdentityDbContext
+    {
+        public CarRentingDbContext(DbContextOptions<CarRentingDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
